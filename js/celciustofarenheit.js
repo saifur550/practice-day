43998fc1f8f0186a2f257859b67f2cf97191eea4
saifur550 <srstap550@gmail.com>
2021-08-05 10:03:12 +0800

    git init

---
+++ js/celciustofarenheit.js
@@ -0,0 +1,17 @@
+
+//celsius to fahrenheit formula
+//input * 9/5 + 32
+// var celsiusNumber = prompt(" Enter the Number");
+var celsiusNumber = 5;
+
+var resultOut = celsiusNumber * 9/5 +32;
+console.log(resultOut);
+
+//fahrenheit to celsius 
+// formula (input-32) * 5/9
+
+
+var farhrenheitNumber  = 10;
+var outResult = (farhrenheitNumber- 32) * 5/9 ;
+console.log(outResult);
+
---
+++ js/grade.js
@@ -0,0 +1,9 @@
+
+// var studentmarks = prompt(" Enter Your Exam marks Number");
+var studentmarks =79;
+
+if (100 < 109){
+    console.log("A+");
+}else{
+    console.log("A");
+}
