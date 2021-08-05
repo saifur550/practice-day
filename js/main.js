5a23f16bac01fcc32f91947ec3537006442e6159
saifur550 <isaifur550@icloud.com>
2021-08-05 19:35:10 +0800

    link added

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//CI = P*(1 + R/n) (nt) – P
+
+// P is the principal amount.
+// R is the annual interest rate.
+// t is the time the money is invested or borrowed for.
+
+//https://skillpundit.com/javascript/javascript-simple-interest-compound-interest.php
